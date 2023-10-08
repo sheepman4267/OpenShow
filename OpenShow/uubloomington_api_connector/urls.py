79cb900c16e5f96fe00a5e2ecf062630c40b94f4
Hans K <hans@communitycomputing.net>
2023-10-08 00:08:26 -0400

    Fix UUCB API connector styling

--- OpenShow/uubloomington_api_connector/urls.py
+++ OpenShow/uubloomington_api_connector/urls.py
@@ -3,6 +3,5 @@ from django.views.generic import TemplateView
 from .views import *
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='uubloomington_api_connector/index.html'), name='uucb-api-index'),
     path('create-show-from-oos/', CreateShowFromOOSView.as_view(), name='create-show-from-oos'),
 ]
