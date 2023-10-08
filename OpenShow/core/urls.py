d46fcae8d4d1beb1efe37059b5151cb5f2084da1
Hans K <hans@communitycomputing.net>
2023-10-08 00:42:43 -0400

    Abuse icon buttons for now, pending complete styling overhaul

--- OpenShow/core/urls.py
+++ OpenShow/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/index.html')),
+    path('', TemplateView.as_view(template_name='core/index.html'), name='index'),
 ]
