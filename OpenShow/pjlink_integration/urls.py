570b5f2740c9ec96b344781f2b0d9b5e08fd6c7b
Hans K <hans@communitycomputing.net>
2023-10-30 21:15:56 -0400

    Fix warning about unnecessary slash

--- OpenShow/pjlink_integration/urls.py
+++ OpenShow/pjlink_integration/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', ProjectorListView.as_view(), name='pjlink-index'),
-    path('/new', ProjectorCreateView.as_view(), name='new-projector'),
-    path('/edit/<int:pk>', ProjectorUpdateView.as_view(), name='edit-projector'),
+    path('new', ProjectorCreateView.as_view(), name='new-projector'),
+    path('edit/<int:pk>', ProjectorUpdateView.as_view(), name='edit-projector'),
     path('power/<int:pk>/on', power_on, name='power-on'),
     path('power/<int:pk>/off', power_off, name='power-off'),
 ]
