7dd8ea6ae258b168a48b973815e32ecdaf803a62
Hans K <hans@communitycomputing.net>
2023-12-10 08:56:58 -0500

    Auto-formatted cues testing

---
+++ format_cues.py
@@ -0,0 +1,12 @@
+# A python script to test logic (and hopefully be useful) for auto-formatting text into cues for decks.
+
+with open('cues.txt', 'r') as cuefile:
+    cuelines = cuefile.readlines()
+
+for line in cuelines:
+    cuewords = line.split(' ')
+    if len(cuewords) > 6:
+        print(f'{cuewords[0]} {cuewords[1]} {cuewords[2]} (...) {cuewords[-3]} {cuewords[-2]} {cuewords[-1]}'.strip('\n'))
+    else:
+        print(line.strip('\n'))
+    print('~~')
