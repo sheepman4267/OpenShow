f04fa19dec32b4547978876893bda6bf6b7735a7
Hans K <hans@communitycomputing.net>
2024-02-07 15:28:14 -0500

    Increase length of cue words

--- format_cues.py
+++ format_cues.py
@@ -5,7 +5,7 @@ with open('cues.txt', 'r') as cuefile:
 
 for line in cuelines:
     cuewords = line.split(' ')
-    if len(cuewords) > 6:
+    if len(cuewords) > 12:
         print(f'{cuewords[0]} {cuewords[1]} {cuewords[2]} (...) {cuewords[-3]} {cuewords[-2]} {cuewords[-1]}'.strip('\n'))
     else:
         print(line.strip('\n'))
