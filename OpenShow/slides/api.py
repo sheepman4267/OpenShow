e207abce08b6022620501e26719d8355e2e3abc3
Hans Kelson <hans@communitycomputing.net>
2024-04-15 22:19:02 -0400

    api: Use POST body rather than URL params for show_slide

--- OpenShow/slides/api.py
+++ OpenShow/slides/api.py
@@ -1,12 +1,18 @@
-from ninja import Router
+from ninja import Router, Schema
 from .models import Slide, Show
 from django.shortcuts import get_object_or_404
 
 router = Router()
 
 
+class SlideAndShowSchema(Schema):
+    slide_pk: int
+    show_pk: int
+
+
 @router.post("show_slide")
-def show_slide(request, slide_pk, show_pk):
-    slide = get_object_or_404(Slide, pk=slide_pk)
-    show = get_object_or_404(Show, pk=show_pk)
+def show_slide(request, slide_and_show: SlideAndShowSchema):
+    slide = get_object_or_404(Slide, pk=slide_and_show.slide_pk)
+    show = get_object_or_404(Show, pk=slide_and_show.show_pk)
     slide.send_to_display(show.displays.all(), show=show)
+    return {"message": "OK"}
