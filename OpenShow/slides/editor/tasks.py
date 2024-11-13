b90880a49738bb4424632dda0bb86342ff07376d
Hans Kelson <hans@communitycomputing.net>
2024-11-13 13:40:12 -0500

    Fix transcode output path for audio
    If you think this looks familiar, that's because it is: 6c06222

--- OpenShow/slides/editor/tasks.py
+++ OpenShow/slides/editor/tasks.py
@@ -1,10 +1,20 @@
-import pathlib
+import pathlib, subprocess, json
 
 from django.conf import settings
 from django.utils.text import slugify
 
 from slides.models import MediaObject
 from ffmpeg import FFmpeg, Progress
+
+def get_mediafile_seconds(media_path):
+    result = subprocess.check_output(
+        f'ffprobe -v quiet -show_streams -select_streams v:0 -of json "{settings.MEDIA_ROOT + media_path}"',
+        shell=True).decode()
+    fields = json.loads(result)['streams'][0]
+
+    duration = fields['tags']['DURATION']
+    # fps = eval(fields['r_frame_rate'])
+    return duration
 
 def transcode_video(media_object_pk: int) -> None:
     media_object = MediaObject.objects.get(pk=media_object_pk)
@@ -36,7 +46,7 @@ def transcode_video(media_object_pk: int) -> None:
 def transcode_audio(media_object_pk: int) -> None:
     media_object = MediaObject.objects.get(pk=media_object_pk)
     final_file_name = slugify(media_object.title) + '.mp3'
-    tmp_transcode_out_path = settings.MEDIA_ROOT + 'media_final/audio/' + final_file_name
+    tmp_transcode_out_path = settings.MEDIA_ROOT + '/media_final/audio/' + final_file_name
     pathlib.Path(tmp_transcode_out_path).parents[0].mkdir(parents=True, exist_ok=True)
     ffmpeg = (
         FFmpeg()
