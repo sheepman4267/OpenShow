cb84f6f34f5fc536bc1179599331cf67ef11d2ce
Hans Kelson <hans@communitycomputing.net>
2025-10-15 17:18:47 -0400

    Refactor media object templates out of the main editor directory

--- OpenShow/slides/editor/views/media.py
+++ OpenShow/slides/editor/views/media.py
@@ -55,7 +55,7 @@ class MediaObjectCRUDView(CRUDView):
 
         if self.model is not None and self.template_name_suffix is not None:
             return [
-                f"editor/"
+                f"editor/media_object/"
                 f"{self.model._meta.object_name.lower()}"
                 f"{self.template_name_suffix}.html",
                 f"neapolitan/object{self.template_name_suffix}.html",
